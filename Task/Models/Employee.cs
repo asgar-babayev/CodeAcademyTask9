c7d2e8af371245dec25cca5749abff9e81e12850
Asger Babayev <asgerbabayev@hotmail.com>
2022-03-18 20:11:00 +0400

    added

---
+++ Task/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task.Models
+{
+    class Employee : Person
+    {
+        public double SalaryOfHour { get; set; }
+        public double WorkingHour { get; set; }
+
+        public Employee(string name, string surname, int age, double salaryOfHour, double workingHour)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            SalaryOfHour = salaryOfHour;
+            WorkingHour = workingHour;
+        }
+
+        public double CalculateSalary()
+        {
+            return SalaryOfHour * WorkingHour;
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = Console.ReadLine();
+            //string Surname = Console.ReadLine();
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //double salaryOfHour = Convert.ToDouble(Console.ReadLine());
+            //double workingHour = Convert.ToDouble(Console.ReadLine());
+           
+        }
+    }
+
+}
